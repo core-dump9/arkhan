c48b00c3e55a583dbc68de2c99a6ceeaf6a4afbf
Akapo Damilola F <fdamilola@gmail.com>
2012-07-21 01:52:12 +0100

    scanf

--- cfun/c241.c
+++ cfun/c241.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 main () {
-	printf("Jenny was a friend of mine\n");
-	exit(0);
+	int senther;
+	scanf("%d", &senther);
+	char zet = senther;
+	printf("%c\n", zet);
 }
--- cfun/lega.c
+++ cfun/lega.c
@@ -1,1 +1,13 @@
 #include <stdio.h>
+
+void get (int a, int b) {
+	int sum;
+	sum = (a+b);
+	printf("%d\n", sum);
+}
+
+int main(int argc, char const *argv[])
+{
+	get(4, 4);
+	return 0;
+}
